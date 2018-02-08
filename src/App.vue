447d55a8a38caa3208bf66510dec5161a8793796
Oleg <reg.barsuk@gmail.com>
2018-02-08 20:41:37 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <p>
+      Styled with a little sass
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+  body {
+    background: blue;
+    p {
+      color: red;
+    }
+  }
+</style>
